cc1fb78d4f8c0fa330149ceb9d454e52c2b99f42
Matthew Villeneuve <matthew.villeneuve@gmail.com>
2018-10-25 06:57:41 -0700

    looking good all around. Start up looks good in rpi.

--- src/hardware.ts
+++ src/hardware.ts
@@ -1,13 +1,15 @@
-import { Gpio, Direction} from 'onoff';
-import { eventBus } from './eventBus';
-import { constants } from './constants';
-import logger from './logger';
+import { Gpio, Direction } from "onoff";
+import { eventBus } from "./eventBus";
+import { constants } from "./constants";
+import logger from "./logger";
 
 /**
  * start the button watcher
  */
 export const watchButton = () => {
-  const button = safeGpio(constants.pins.button, 'in', 'both', { debounceTimeout: 250 });
+  const button = safeGpio(constants.pins.button, "in", "both", {
+    debounceTimeout: 250
+  });
 
   logger.info(`button.watch: start watcher`);
 
@@ -20,28 +22,34 @@ export const watchButton = () => {
     eventBus.emit(constants.events.BUTTON_PRESSED);
   });
 
-  process.on('SIGINT', () => {
+  process.on("SIGINT", () => {
     button.unexport();
   });
-
 };
-export const safeGpio = (gpio: number, direction: Direction, edge?: String, options?: Object): Gpio => {
-  if (Gpio.accessable) {
+export const safeGpio = (
+  gpio: number,
+  direction: Direction,
+  edge?: String,
+  options?: Object
+): Gpio => {
+  const osType = require("os").type();
+  if (osType == "Linux") {
     logger.info(`Gpio is available. Setting up pin: ${gpio}`);
 
-    return new Gpio(gpio, direction, edge, options );
+    return new Gpio(gpio, direction, edge, options);
   } else {
     // @ts-ignore
     return {
-      watch: (cb) => {
+      watch: cb => {
         setInterval(() => {
-          logger.info('keep app alive');
+          logger.info("keep app alive");
         }, 100000);
-
       },
       unexport: () => {},
-      readSync: () => {return 1},
+      readSync: () => {
+        return 1;
+      },
       writeSync: () => {}
-    }
+    };
   }
-}
+};
